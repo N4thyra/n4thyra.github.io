93eafb185fb40528d2721f8e0d567a5988c3fbda
Jiri Laza <N4thyra@users.noreply.github.com>
2017-08-12 16:24:05 +0200

    added burger functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 $('document').ready(()=> {
   const imageFooter = $('.portfolio-footer-image');
+  if(window.innerWidth > 701) {
+    if(!$('#nav-checkbox').is(':checked')) {
+      $('#nav-checkbox').attr('checked', true);
+    }
+  }
 
-imageFooter.on('mouseenter', function() {
-    $(this).effect('bounce');
-  });
+  function checkWidth() {
+    if(window.innerWidth > 701) {
+      if(!$('#nav-checkbox').is(':checked')) {
+        $('#nav-checkbox').attr('checked', true);
+      }
+    } else {
+      $('#nav-checkbox').attr('checked', false);
+    }
+  }
+
+  function windowWidth() {
+    if(window.innerWidth > 701) {
+      if(!$('#nav-checkbox').is(':checked')) {
+        $('#nav-checkbox').attr('checked', true);
+      }
+    }
+  }
+
+  $(window).resize(checkWidth);
+
+  imageFooter.on('mouseenter', function() {
+      $(this).effect('bounce');
+    });
 });
